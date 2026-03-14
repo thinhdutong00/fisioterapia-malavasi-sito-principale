129090898515826b59b9f36d0b1131e57000d34c
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-03-14 17:33:34 +0100

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
