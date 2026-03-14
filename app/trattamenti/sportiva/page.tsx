bb9b9d042450d9e344882ca25851840d44ebc4f5
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-03-15 00:31:53 +0100

    fix

--- app/trattamenti/sportiva/page.tsx
+++ app/trattamenti/sportiva/page.tsx
@@ -91,7 +91,7 @@ export default function FisioterapiaSportiva() {
               <div className="grid lg:grid-cols-2 gap-16 items-center">
                 <div>
                   <h2 className="text-3xl md:text-5xl font-black text-[#022166] mb-8 tracking-tight">
-                    Un approccio <br />sartoriale.
+                    Come lo <br />facciamo
                   </h2>
                   <div className="space-y-8">
                     <div className="flex gap-5">
